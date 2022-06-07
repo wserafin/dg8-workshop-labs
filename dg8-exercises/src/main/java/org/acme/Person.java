bd25b4b73c61ce7b48f8b5fb2ed79eabe4b461a5
wserafin <wserafin@redhat.com>
2022-06-07 15:23:47 -0400

    Update Person.java

--- dg8-exercises/src/main/java/org/acme/Person.java
+++ dg8-exercises/src/main/java/org/acme/Person.java
@@ -5,21 +5,28 @@ import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.Store;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+
 @Indexed
 public class Person {
-    @Field(store = Store.YES, analyze = Analyze.NO)
-    String name;
 
-    @Field(store = Store.YES, analyze = Analyze.NO, indexNullAs = Field.DEFAULT_NULL_TOKEN)
-    String surname;
+ 
 
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
+   @KeywordField
+   String name;
 
-    @Override
-    public String toString() {
-        return "Person [name=" + name + ", surname=" + surname + "]";
-    }
+   @KeywordField
+   String surname;
+
+   public Person(String name, String surname) {
+      this.name = name;
+      this.surname = surname;
+   }
+
+   @Override
+   public String toString() {
+      return "Person [name=" + name + ", surname=" + surname + "]";
+   }
+
 }
